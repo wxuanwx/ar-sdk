8edd0c07a401412d8cac083fec840fa8fa394803
wangxuan <wxuanwxwxwx@gmail.com>
2026-08-25 16:02:21 +0800

    Install JitPack root and module artifacts

--- jitpack-install.sh
+++ jitpack-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readonly repository_group="${GROUP:-com.github.wxuanwx}.${ARTIFACT:-ar-sdk}"
+readonly root_group="${GROUP:-com.github.wxuanwx}"
+readonly repository_group="${root_group}.${ARTIFACT:-ar-sdk}"
 readonly repository_artifact="${ARTIFACT:-ar-sdk}"
 readonly release_version="${VERSION:-1.0.0}"
 readonly local_repository="${HOME}/.m2/repository"
@@ -28,6 +29,7 @@ readonly artifacts=(
 )
 
 group_path="${repository_group//./\/}"
+root_group_path="${root_group//./\/}"
 
 write_pom() {
   local target="$1"
@@ -59,7 +61,21 @@ for artifact in "${artifacts[@]}"; do
   printf 'Installed %s:%s:%s\n' "$repository_group" "$artifact_id" "$release_version"
 done
 
-aggregate_dir="${local_repository}/${group_path}/${repository_artifact}/${release_version}"
+cat > pom.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>${root_group}</groupId>
+  <artifactId>${repository_artifact}</artifactId>
+  <version>${release_version}</version>
+  <packaging>pom</packaging>
+  <name>AR SDK binary artifacts</name>
+</project>
+EOF
+
+aggregate_dir="${local_repository}/${root_group_path}/${repository_artifact}/${release_version}"
 aggregate_pom="${aggregate_dir}/${repository_artifact}-${release_version}.pom"
 mkdir -p "$aggregate_dir"
 cat > "$aggregate_pom" <<EOF
@@ -68,7 +84,7 @@ cat > "$aggregate_pom" <<EOF
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
   <modelVersion>4.0.0</modelVersion>
-  <groupId>${repository_group}</groupId>
+  <groupId>${root_group}</groupId>
   <artifactId>${repository_artifact}</artifactId>
   <version>${release_version}</version>
   <packaging>pom</packaging>
